55cccc367265ff230da9675c830cd12e7d224dbe
Franky Naidu <frankynaidu16081996@gmail.com>
2018-10-02 01:34:23 +0530

    Added a div block

--- index.php
+++ index.php
@@ -67,6 +67,22 @@
              </div>
           </div>
         </div>
+       
+		 class="col s12 m4 ">
+          <div class="card hoverable">
+            <div class="card-image">
+              <img src="images/cyber.png">
+              <span class="card-title">Synergy</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+ <a class="waves-effect waves-light btn indigo darken-4">Know more</a>
+             </div>
+          </div>
+        </div>
 
          <div class="col s12 m4 ">
           <div class="card hoverable">
